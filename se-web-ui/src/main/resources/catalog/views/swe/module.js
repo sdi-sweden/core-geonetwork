a75cbc0e3cd9f5c0c2e7221def7e43c60173bb85
Michel Gabriel <michel.gabriel@geocat.net>
2016-12-22 09:05:04 +0100

    Final styles for the responsive design: - menu on tablets without text, only icons - option for smaller maps on a tablet
    Fix for settings menu showing behind the layer panel in the map
    Issue: https://app.assembla.com/spaces/kcubemetagis/tickets/realtime_list?ticket=3709

--- se-web-ui/src/main/resources/catalog/views/swe/module.js
+++ se-web-ui/src/main/resources/catalog/views/swe/module.js
@@ -483,7 +483,17 @@
         $scope.vectorLayer.getSource().addFeature(feature);
       };
 
-     
+      /**
+       * Toggle size of floating map
+       */
+      $scope.toggleFloatingMap = function() {
+        angular.element('.floating-map-cont').toggleClass('small');
+        // angular.element('.floating-map-cont').show();
+        // $scope.$emit('body:class:remove', 'small-map-view');
+        // $scope.$emit('body:class:remove', 'full-map-view');
+        // $scope.$emit('body:class:remove', 'medium-map-view');
+      };
+
       /**
        * Show full view results.
        */
