f21d665ba0e8830f3bf39fae7fa56b1080cbf406
petbod <peter-alexander.bodley@lm.se>
2019-02-12 14:20:32 +0100

    final cleanup

--- cucumber_e2e/protractor-conf.js
+++ cucumber_e2e/protractor-conf.js
@@ -1,24 +1,31 @@
 
 exports.config = {
-  baseUrl: 'http://tst.geodata.se/geodataportalen/srv/swe/catalog.search?',
+  baseUrl: 'https://tst.geodata.se/geodataportalen/srv/swe/catalog.search#/home',
+//  baseUrl: 'http://localhost:8080/geodataportalen',
   seleniumAddress: 'http://localhost:4444/wd/hub',
   getPageTimeout: 60000,
-  allScriptsTimeout: 300000,
+  allScriptsTimeout: 240000,
   framework: 'custom',
   frameworkPath: 'node_modules/protractor-cucumber-framework',
-  capabilities: {
-    browserName:'chrome',
-    args: ['no-sandbox']
-  },
-  
+  multiCapabilities: 
+    {
+	  shardTestFiles: true,
+	  browserName: 'chrome',
+	  chromeOptions: {
+            args: [
+//                   'headless', 
+			       'disable-gpu',			
+				   'no-sandbox'
+				  ]
+        }	  
+    },
   specs: [
-
-	'features/HanteraFavoriter.feature',
+    'features/HanteraFavoriter.feature',
 	'features/Sok_Enkel_vy.feature',
 	'features/VisaResultat.feature',
-	'features/Sok_Avancerad_vy.feature',
-	'features/VisaMetadata.feature',
-	'features/Sok_Generellt.feature',
+    'features/Sok_Avancerad_vy.feature',
+	'features/VisaMetadata.feature',	
+    'features/Sok_Generellt.feature',
 	'features/VisaDoljFiltrering.feature',
 	'features/VisaHjalptexter.feature',
 	'features/VisaKarttjanster.feature',
@@ -46,8 +53,21 @@ exports.config = {
 
     strict: true,
 	failfast: true,
-    format: 'pretty',
-	keepAlive: true,
+    format: ['pretty', 'json:reports/report.json'],
+	keepAlive: false,
+	ignoreUncaughtExceptions: true,
+	untrackOutstandingTimeouts: true,
     tags: '@qa_ready'  
   }
+  ,
+  plugins: [{
+        package: 'protractor-multiple-cucumber-html-reporter-plugin',
+        options:{
+		    automaticallyGenerateReport: true,
+            removeExistingJsonReportFile: true,
+			jsonOutputPath: 'reports/',
+			openReportInBrowser: true
+        }
+  }]
+  
 };
